5c35acc6ced48696974eab6cb785556467363cc9
Emmanuel Blancquart <big_pannard@mgn.com>
2025-10-22 19:48:17 +0200

    Initialize

---
+++ cp_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#####################################################################
+# Author:Emmanuel Blancquart
+# mail:big_pannard@msn.com
+# cp_image.sh --source [folderPath] --destination [DestinationPath]
+# --Source Folder Source
+# --Destination Folder Destination
+#####################################################################
+
